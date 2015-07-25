7f8d7e436649da55815b52f4e696b03285fcbc67
Zach Galant <zach@codehs.com>
2015-07-25 16:10:12 -0700

    typo fix

--- send_recieve.js
+++ send_recieve.js
@@ -31,7 +31,7 @@ function createUserRow(user){
 * @param decodedMessage - string indicating decoded message
 */
 function createInboxTableRow(from, message, decodedMessage){
-	return "<tr><td>" + from + "</td><td>" + message + "</td><td>" + decoded + "</td></tr>";
+	return "<tr><td>" + from + "</td><td>" + message + "</td><td>" + decodedMessage + "</td></tr>";
 }
 
 function sendMessage(from, to, message){
